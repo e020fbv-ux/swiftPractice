ea7f69710082ac439c7ce39166ef77f4d386bc14
e020fbv-ux <e020fbv@yamaguchi-u.ac.jp>
2025-09-01 16:57:52 +0900

    Initial commit

---
+++ swiftPractice/swiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftPracticeApp.swift
+//  swiftPractice
+//
+//  Created by 小田貴将 on 2025/09/01.
+//
+
+import SwiftUI
+
+@main
+struct swiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftPracticeTests/swiftPracticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  swiftPracticeTests.swift
+//  swiftPracticeTests
+//
+//  Created by 小田貴将 on 2025/09/01.
+//
+
+import Testing
+
+struct swiftPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
